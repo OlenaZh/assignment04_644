7b11a54edf89f79d4bea88db7cd0901715ef6403
OlenaZh <alyona.mesh@gmail.com>
2022-03-16 16:37:55 -0700

    S1-done

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,5 +1,13 @@
 //STEP 1
+//let number;
 
+//function halfNumber() {
+   // number = number / 2;
+    //document.write(`Half is ${number}.`);
+//}
+
+//number = parseFloat(prompt('Please enter a number'));
+//halfNumber();
 
 
 //STEP 2
