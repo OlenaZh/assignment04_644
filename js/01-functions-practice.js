c67c4aaffa0490fbaf3d9210e125e1e03820e348
OlenaZh <alyona.mesh@gmail.com>
2022-03-16 17:08:18 -0700

    S3 - done

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -10,17 +10,28 @@
 //halfNumber();
 
 //STEP 2
-let number;
+//let number;
 
-function squareNumber() {
-    number = number * number;
-   document.write(`The result of squaring the number is ${number}.`);
-}
+//function squareNumber() {
+    //number = number * number;
+   //document.write(`The result of squaring the number ///is ${number}.`);
+//}
 
-number = parseFloat(prompt('Please enter a number'));
-squareNumber();
+//number = parseFloat(prompt('Please enter a number'));
+//squareNumber();
 
 //STEP 3
+let result, firstNumber, secondNumber;
+
+function percentOf() {
+    result = (firstNumber / secondNumber) * 100;
+    document.write(`${firstNumber} is ${result}% of ${secondNumber}`);
+}
+
+firstNumber = parseFloat(prompt('Please enter a first number'));
+secondNumber = parseFloat(prompt('Please enter a second number'));
+percentOf();
+
 
 //STEP 4
 
